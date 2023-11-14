b31289b6c8269a86ebb09ae1e7bcb795ca5415a6
Thore Müller <75487485+LUPLUV@users.noreply.github.com>
2023-11-14 19:00:13 +0100

    fix? half

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -30,13 +30,13 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
     public function authenticate(Request $request): Passport
     {
-        $email = $request->request->get('email', '');
+        $email = $request->request->get('_email', '');
 
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
         return new Passport(
             new UserBadge($email),
-            new PasswordCredentials($request->request->get('password', '')),
+            new PasswordCredentials($request->request->get('_password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->get('_csrf_token')),
             ]
